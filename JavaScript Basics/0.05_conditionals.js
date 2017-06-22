07d99fa6bad8ce46ca1254f950788ab1243f714a
Christian <tehdovebusiness@gmail.com>
2017-06-22 09:32:36 -0400

    new Java

--- JavaScript Basics/0.05_conditionals.js
+++ JavaScript Basics/0.05_conditionals.js
@@ -10,6 +10,17 @@ if (age == 18){
 var password= "thisismypassword"
 if (password == "thisismypassword"){
 	console.log("Welcome stranger");
-} else {
+} else {   
 	console.log("No access for you");
 }
+//Bronze
+var age=27;
+if (age >= 60) {
+	console.log("Yes! you can now retire!")
+}	else if (age >= 40){
+	console.log("You now are a self-sustained individual")
+}	else if (age >= 27) {
+	console.log("You can now find a good career")
+}	else if (age >= 18) {
+	console.log("You can now start college and begin the path to success")
+}	else if (age <= 18) {"You are still a child, but just keep moving on ahead"}
